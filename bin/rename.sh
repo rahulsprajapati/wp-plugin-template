a295b394f274a3793455a90ba91ff4d876b8cfd4
Rahul Prajapati <rahul.prajapati@live.in>
2021-09-29 13:24:26 +0530

    Update rename script.

--- bin/rename.sh
+++ bin/rename.sh
@@ -18,21 +18,15 @@ for arg; do
 	esac
 done
 
-if [ -z "$SLUG" ]; then
-	echo "Slug not found, Please provide --slug arg to procced with rename..."
-	exit;
+if [ -n "$SLUG" ]; then	
+	echo "Updating plugin slug with $SLUG"
+	grep -rl wp-plugin-template . --exclude-dir={node_modules,vendor,.history,.git,bin} | xargs sed -i '' "s/wp-plugin-template/$SLUG/g"
+
+	echo "Updating plugin files with $SLUG"
+	find . -name 'wp-plugin-template*' -type f | sed -e "p;s/wp-plugin-template/$SLUG/"  | xargs -n2 mv
 fi
 
-if [ -z "$NAMESPACE" ]; then
-	echo "Namespace not found, Please provide --namespace arg to procced with rename..."
-	exit;
+if [ -n "$NAMESPACE" ]; then
+	echo "Updating plugin namespace with $NAMESPACE"
+	grep -rl WP_Plugin_Template . --exclude-dir={node_modules,vendor,.history,.git,bin} | xargs sed -i '' "s/WP_Plugin_Template/$NAMESPACE/g"
 fi
-
-echo "Updating plugin slug with $SLUG"
-grep -rl wp-plugin-template . --exclude-dir={node_modules,vendor,.history,.git,bin} | xargs sed -i '' "s/wp-plugin-template/$SLUG/g"
-
-echo "Updating plugin files with $SLUG"
-find . -name 'wp-plugin-template*' -type f | sed -e "p;s/wp-plugin-template/$SLUG/"  | xargs -n2 mv
-
-echo "Updating plugin namespace with $NAMESPACE"
-grep -rl WP_Plugin_Template . --exclude-dir={node_modules,vendor,.history,.git,bin} | xargs sed -i '' "s/WP_Plugin_Template/$NAMESPACE/g"
